86461202379ae5c31260ebea17ae3f1e3d6f6ff1
Luciano Tavares <lucianomt@gmail.com>
2023-02-22 10:58:58 -0300

    restores previous style

--- app/Prompt.jsx
+++ app/Prompt.jsx
@@ -27,29 +27,26 @@ export default function Prompt({ question }) {
   return (
     <header className="flex flex-col justify-center items-center w-full mt-20 mb-8">
       <form onSubmit={handleSubmit} className="w-full max-w-screen-md flex flex-col items-center">
-        <div className="relative w-full">
-          <label htmlFor="prompt" className="sr-only">
-            Me pergunte qualquer coisa.
-          </label>
-          <input
-            type="text"
-            id="prompt"
-            value={prompt}
-            placeholder={placeholder}
-            className="w-full p-4 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-xl"
-            onChange={handleChange}
-          />
-          <button
-            type="submit"
-            className={`absolute top-0 right-0 h-full px-4 rounded-r-lg bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-white ${
-              isLoading ? "bg-gray-300 text-blueGray-600" : ""
-            }`}
-            style={{ width: "110px" }}
-            disabled={isLoading}
-          >
-            {isLoading ? "Carregando..." : "Enviar"}
-          </button>
-        </div>
+        <label htmlFor="prompt">
+          <span className="sr-only">Me pergunte qualquer coisa.</span>
+        </label>
+        <input
+          type="text"
+          id="prompt"
+          value={prompt}
+          placeholder={placeholder}
+          className="w-full p-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 text-xl"
+          onChange={handleChange}
+        />
+        <button
+          type="submit"
+          className={`mt-4 px-16 py-3 rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-white ${
+            isLoading ? "bg-gray-300" : "bg-blue-600 text-white hover:bg-blue-700"
+          }`}
+          disabled={isLoading}
+        >
+          {isLoading ? "Carregando..." : "Enviar"}
+        </button>
       </form>
     </header>
   )
