ba8086c4a01faf42440cacfafa25644a14d1130a
Luciano Tavares <lucianomt@gmail.com>
2023-02-21 23:10:52 -0300

    removes log

--- app/Response.jsx
+++ app/Response.jsx
@@ -1,6 +1,4 @@
 export default function Response({ response }) {
-  console.log("Response: ", response.data)
-
   return (
     <div className="w-full max-w-screen-md mx-auto my-8">
       <p className="text-lg">{response.data.choices[0].text}</p>
