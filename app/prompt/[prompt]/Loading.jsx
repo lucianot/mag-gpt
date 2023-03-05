9fd6fa7bfef5cad57437dab0d999ecd729e53e44
Luciano Tavares <lucianomt@gmail.com>
2023-03-05 16:55:43 -0300

    added loading animations

--- app/prompt/[prompt]/Loading.jsx
+++ app/prompt/[prompt]/Loading.jsx
@@ -1,7 +1,13 @@
 export default function Loading() {
   return (
     <div className="mt-10 w-full max-w-[650px]">
-      <div className="text-2xl text-gray-400 mb-2">Pesquisando...</div>
+      <div className="font-bold text-2xl text-blue-600 mb-2">Mag-GPT:</div>
+      <div className="animate-pulse mt-2">
+        <div className="h-4 bg-gray-300 rounded mt-3"></div>
+        <div className="h-4 bg-gray-300 rounded mt-3"></div>
+        <div className="h-4 bg-gray-300 rounded mt-3"></div>
+        <div className="h-4 bg-gray-300 rounded mt-3"></div>
+      </div>
     </div>
   )
 }
